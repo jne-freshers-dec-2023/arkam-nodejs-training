bd19c5f1f904c61bdce414072fb43d4226ac8fbb
LNV-258 <arkam.patel@thinkitive.com>
2023-12-27 20:33:50 +0530

    fundamental express

---
+++ TestModeule2.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+const requestedFuntion = (req, res)=>
+{
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/')
+    {
+        res.write('<html>')
+        res.write('<head><title>Node JS</title></head>')
+        res.write(
+            '<body> <form action="/massage" method="POST"><input type = "text" name = "myMassage"><button type = "submit">Submit</button></form></bosy>'
+        );
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/massage' && method === 'POST')
+    {
+        const body = [];
+
+        req.on('data',(piece)=>
+        {
+            body.push(piece);
+        })
+
+        req.on('end',()=>
+        {
+            const myData = Buffer.concat(body).toString();
+            console.log(myData)
+            fs.writeFileSync('demo.txt',myData)
+
+        })
+        res.statusCode = 302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<header><title>Node JS Response</title><header>');
+    res.write('<body>Hello From Arkam</body>');
+    res.write('</html>')
+    res.end();
+}
+
+//module.exports = requestedFuntion;
+
+// module.exports = 
+// {
+//     handler: requestedFuntion,
+// }
+
+exports.requestedFuntion;
+exports.name = 'Arkam';
