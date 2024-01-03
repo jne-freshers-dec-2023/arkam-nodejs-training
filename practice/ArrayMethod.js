ed0180bc08182f85e30b248ed2e5aeda7f2e7f41
LNV-258 <arkam.patel@thinkitive.com>
2024-01-03 20:45:18 +0530

    mongo user

---
+++ practice/ArrayMethod.js
@@ -0,0 +1,110 @@
+let priceOfFruits = 
+[
+    { name: "mango", value:240},
+    { name: "apple", value:40},
+    { name: "jackfruit", value:890},
+    { name: "lemon", value:280},
+    { name: "watermelon", value:20},
+    { name: "orange", value:210},
+    { name: "banana", value:150}
+]
+
+    // Filter Method
+
+// let filteredArray = priceOfFruits.filter((item)=>
+// {
+//     return (item.value >=100)
+// })
+
+// console.log("Filtered Array: ",filteredArray)
+
+
+
+        //map method
+
+// let mappedArray1 = priceOfFruits.map((item)=>
+// {
+//     return item.name
+// })
+// console.log("Mapped Array: ",mappedArray1)
+
+
+// let mappedArray2 = priceOfFruits.map((item)=>
+// {
+//     return item.value
+// })
+// console.log("Mapped Array: ",mappedArray2)
+
+
+    //find Method
+
+    // let findMethod = priceOfFruits.find((item)=>
+    // {
+    //     return item.name === "mango"
+    // })
+    // console.log(findMethod)
+
+
+    // ForEach
+
+// priceOfFruits.forEach((eachItem)=>
+// {
+//     console.log(eachItem)
+// }) 
+
+
+//         //some
+// let moreThan100 = priceOfFruits.some((item)=>
+// {
+//     return item.value > 100;
+// })
+
+// console.log(moreThan100)
+
+//         //every
+// let moreThan100 = priceOfFruits.every((item)=>
+// {
+//     return item.value > 10;
+// })
+// console.log(moreThan100)
+
+
+///         reduce
+// let total = priceOfFruits.reduce((sum,item) =>
+// {
+//     return sum = sum + item.value
+// },0)
+// console.log(total)
+
+
+let arr = [ 10,20,30,40,50,60,70,80]
+
+// includes
+
+// let value = arr.includes(3);
+// console.log(value)
+
+
+// indexof
+
+// let value = arr.indexOf(50)
+// console.log(value)
+
+
+//findIndex
+
+// let value = arr.findIndex((item)=>
+// {
+//     return item === 80
+// })
+// console.log(value)
+
+
+// findIndex
+
+let value = priceOfFruits.find((item)=>
+{
+    return item.value === 40 
+})
+console.log(value)
+
---
+++ practice/Login.js
@@ -0,0 +1,17 @@
+const exress = require('express');
+const mongoose = require('mongoose')
+const loginRoutes = require('../Routes/DBlogin')
+const app = exress();
+
+app.use(loginRoutes)
+
+// app.listen(8080,()=>
+// {
+//     console.log("listning 8080")
+// })
+
+mongoose.connect("mongodb://localhost/test").then(
+    app.listen(8000,() =>{
+        console.log("server is listening on port :",8000);
+    })
+).catch(err => console.log(err))
